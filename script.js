11a7cd6a9af36f8ba7bfb0e5538aa4d4b2ae18d2
totoa553 <77469536+totoa553@users.noreply.github.com>
2022-05-01 11:47:14 +0000

    番号のチェンジ

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const app = new PIXI.Application({width:400,height:300,transparent:true});
-    document.body.appendChild( app.view );
-    var texture = PIXI.Texture.from('img/spritesheet.png');
-    var sprite1 = new PIXI.Sprite(texture);
-    app.stage.addChild( sprite1 );
+const app = new PIXI.Application({width:600,height:600,backgroundAlpha:0});
+document.body.appendChild( app.view );
+var calci = 0;
+PIXI.Loader.shared
+  .add("textures","img/spritesheet.json")
+  .load(setup);
+function setup(){
+  const sprite = PIXI.Sprite.from( calci + ".png" );
+  exSprite = sprite;
+  app.stage.addChild( sprite );
+}
+window.addEventListener("keydown", Keyput);
+var i = 0;
+function Keyput(event){
+  var keyCode = event.keyCode;
+  if (keyCode == 39) {
+    i=i+1;
+    calci = i;
+    app.stage.removeChild(exSprite);
+    if(i === 10){
+      calci = "n";
+      app.stage.removeChild(exSprite);
+    }else if(i===11){
+      i=0;
+      calci = 0;
+      app.stage.removeChild(exSprite);
+    }
+    console.log(calci);
+    setup();
+  }
+  if (keyCode == 37) {
+    aim();
+  }
+}
+function aim(){
+  console.log("aim");
+}
